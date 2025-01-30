50cd204f2cf7421ac3910e726f9b05d9b56383f9
John Lingenfelser <johnlingenfelser@gmail.com>
2025-01-30 13:00:23 -0500

    Update foundryvtt-setup.sh

--- foundryvtt-setup.sh
+++ foundryvtt-setup.sh
@@ -32,7 +32,10 @@ read -p 'Enter the FoundryVTT download URL: ' FOUNDRY_URL
 echo "Setting up first install..."
 
 # Setup Node prerequisites
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
 
 # Setup Caddy prerequisites
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
